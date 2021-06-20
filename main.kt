d2ed4a09f81e0039bf82f0d09754ce3dec7a899a
ItzzCode <51547168+ItzzCode@users.noreply.github.com>
2021-06-20 12:17:25 -0400

    Placeholder

---
+++ main.kt
@@ -0,0 +1,11 @@
+fun op(operator: String){
+	when(operator){
+		"+" -> println("")
+	}
+}
+
+fun main(){
+	println("place holder :)")
+}
+
+//Its all place holder its 12:40 AM (00:40) and i know barely any kotlin AAAAA
