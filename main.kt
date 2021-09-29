2ec54b9b462d930595e18bcaf745cb4f43cf064b
ItzzCode <51547168+ItzzCode@users.noreply.github.com>
2021-09-29 17:43:13 -0400

    stuff

--- main.kt
+++ main.kt
@@ -1,11 +1,7 @@
-fun op(operator: String){
-	when(operator){
-		"+" -> println("")
-	}
-}
-
 fun main(){
-	println("place holder :)")
+	println("Enter equation in A*B format. Enter nothing to exit.")
+	print(readLine())
 }
 
 //Its all place holder its 12:40 AM (00:40) and i know barely any kotlin AAAAA
+//^ please disregard past me
